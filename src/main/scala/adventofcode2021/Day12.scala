b2983ab4f0290dd79047740d4cb86e48bddd5ea0
Phillip Perks <phillipjperks@gmail.com>
2021-12-27 18:06:26 +0000

    Day 12 part 2 solution

--- src/main/scala/adventofcode2021/Day12.scala
+++ src/main/scala/adventofcode2021/Day12.scala
@@ -1,6 +1,7 @@
 package adventofcode2021
 
 import scala.annotation.tailrec
+import scalaz.Scalaz._
 
 object Day12 extends App {
 
@@ -8,6 +9,8 @@ object Day12 extends App {
   val caveConnections = createConnections(input)
   val allRoutes = findAllRoutes(caveConnections)
   println(allRoutes.size)
+  val allRoutesPart2 = findAllRoutesPart2(caveConnections)
+  println(allRoutesPart2.size)
 
   def createConnections(input: List[String]): Map[String, Set[String]] = {
 
@@ -46,6 +49,37 @@ object Day12 extends App {
           val nextCaves = caveConnections(route.last)
           val updatedRoutes = nextCaves.toList.flatMap { cave =>
             if (cave.forall(_.isLower) && route.contains(cave))
+              None
+            else
+              Some(route ++ Seq(cave))
+          }
+          updatedRoutes
+        }
+        traverseCaves(endedRoutes ++ updatedInpProgressRoutes, caveConnections)
+      }
+    }
+
+    traverseCaves(List(List("start")), caveConnections)
+  }
+
+  def findAllRoutesPart2(caveConnections: Map[String, Set[String]]): List[List[String]] = {
+
+    @tailrec
+    def traverseCaves(routes: List[List[String]], caveConnections: Map[String, Set[String]]): List[List[String]] = {
+      if (routes.forall(_.last == "end"))
+        routes
+      else {
+        val endedRoutes = routes.filter(_.last == "end")
+        val inProgressRoutes = routes.filterNot(_.last == "end")
+        val updatedInpProgressRoutes = inProgressRoutes.flatMap { route =>
+          val nextCaves = caveConnections(route.last)
+
+          val updatedRoutes = nextCaves.toList.flatMap { cave =>
+            val smallCavesCount = route.filter(_.forall(_.isLower)).groupBy(c => c).mapValues(_.size) |+| Map(cave -> 1)
+            val revisitedSmallCave = smallCavesCount.values.exists(_ == 3) || smallCavesCount.values.count(_ == 2) > 1
+            if(cave == "start" && route.count(_ == "start") == 1)
+              None
+            else if (cave.forall(_.isLower) && revisitedSmallCave)
               None
             else
               Some(route ++ Seq(cave))
